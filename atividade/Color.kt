b8153bd5694ea4bf53e5ba3b9ef517e18a747577
João Pedro bernardo ribeiro <jpbr@discente.ifpe.edu.br>
2026-03-26 10:23:23 -0300

    Add files via upload

---
+++ atividade/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composenavigationapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val cor1A = Color(0xFFD0BCFF)
+val cor2A = Color(0xFFCCC2DC)
+val cor3A = Color(0xFFEFB8C8)
+
+val cor1B = Color(0xFF6650a4)
+val cor2B = Color(0xFF625b71)
+val cor3B = Color(0xFF7D5260)
